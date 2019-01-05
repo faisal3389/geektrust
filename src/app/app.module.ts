6fb63c0cac1420458c81dcddd905c5159202ec90
Faisal Ali Siddiqui <faisal.siddiqui@capillarytech.com>
2019-01-05 16:52:03 +0530

    added home and result ocmponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,36 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ResultComponent } from './result/result.component';
+
+const appRoutes: Routes = [
+  { path: 'home', component: HomeComponent },
+  {
+    path: 'result',
+    component: ResultComponent,
+    data: { success: 'success' }
+  },
+  { path: '',
+    redirectTo: '/home',
+    pathMatch: 'full'
+  },
+  { path: '**', component: HomeComponent }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ResultComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true }
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
