14fcda1da8f6b8e5bbc700ca09705b39c88b0cca
Faisal Ali Siddiqui <faisal.siddiqui@capillarytech.com>
2019-01-06 16:45:17 +0530

    using view child to get update planets list

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
-
+  @Output() resetClicked = new EventEmitter();
   constructor() { }
 
   ngOnInit() {
@@ -14,5 +14,6 @@ export class HeaderComponent implements OnInit {
   
   reset() {
     console.log('reset pressed');
+    this.resetClicked.emit();
   }
 }
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation, ViewChild } from '@angular/core';
 import { FalconeServiceService } from './../falcone-service.service';
 import { Router } from '@angular/router';
 
@@ -10,11 +10,24 @@ import { Router } from '@angular/router';
 })
 export class HomeComponent implements OnInit {
 
-  disableFind = true;
+  disableFind = false;
   allPlanets = [];
-  unselectedPlanetNames = [];
+  vehicles = [];
+
   allPlanetsNames = [];
-  selectedList = {};
+
+  selectedPlanets = {
+    d1: [],
+    d2: [],
+    d3: [],
+    d4: []
+  }
+  @ViewChild('dest1') dest1;
+  @ViewChild('dest2') dest2;
+  @ViewChild('dest3') dest3;
+  @ViewChild('dest4') dest4;
+
+
   constructor(
     private fService: FalconeServiceService,
     private router: Router
@@ -26,30 +39,46 @@ export class HomeComponent implements OnInit {
         this.allPlanets = pData;
         this.allPlanets.forEach(spaceBall => {
           this.allPlanetsNames.push(spaceBall.name);
-          this.unselectedPlanetNames.push(spaceBall.name);
         })
+        this.updateDestinationChoices(this.allPlanetsNames);
       });
+    this.fService.getVehicles()
+      .subscribe(vData => {
+        this.vehicles = vData;
+      })
+  }
+
+  updateDestinationChoices(pList) {
+    pList.forEach(element => {
+      this.selectedPlanets.d1.push(element);
+      this.selectedPlanets.d2.push(element);
+      this.selectedPlanets.d3.push(element);
+      this.selectedPlanets.d4.push(element);
+    });
   }
 
   planetSelected(...args) {
-    console.log(args);
-    this.selectedList[args[0]] = args[1].value;
-    // this.unselectedPlanetNames = [];
-    // this.allPlanetsNames.forEach((planetName) => {
-    //   if(Object.values(this.selectedList).indexOf(planetName) === -1) {
-    //     this.unselectedPlanetNames.push(planetName);
-    //   }
-    // })
+    for(var i=1; i<=4; i++) {
+      if (args[0] != i) {
+        var index = this.selectedPlanets['d'+i].indexOf(args[1].value);
+        this.selectedPlanets['d'+i].splice(index, 1);
+      }
+    }
   }
 
+  makeSelectedPlanetsBody() {
+    let finalPlanets = [];
+    finalPlanets.push(this.dest1.value);
+    finalPlanets.push(this.dest2.value);
+    finalPlanets.push(this.dest3.value);
+    finalPlanets.push(this.dest4.value);
+    return finalPlanets;
+  }
+  
   findFalcone() {
+    
     let body = {
-      'planet_names': [
-        "Donlon",
-        "Enchai",
-        "Jebing",
-        "Sapir"
-      ],
+      'planet_names': this.makeSelectedPlanetsBody(),
       "vehicle_names": [
         "Space pod",
         "Space rocket",
@@ -64,6 +93,10 @@ export class HomeComponent implements OnInit {
         this.router.navigate(['/result'], { queryParams: { success: false } });
       }
     })
+  }
+
+  reset() {
+    console.log('refresh clicked');
   }
 
 }
